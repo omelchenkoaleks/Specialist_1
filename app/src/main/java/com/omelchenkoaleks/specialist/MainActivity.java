c966ac541495c550d39ff31f6247964a5735c2e6
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-08 11:18:50 +0200

    Методы жизненного цикла = хорошо просматривать с помощью Toast...

--- app/src/main/java/com/omelchenkoaleks/specialist/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/specialist/MainActivity.java
@@ -11,13 +11,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Это обязательный метод, потому-что в суперклассе происходит инициализация
-        // всего на свете, и сами мы это сделать не можем!!!
         super.onCreate(savedInstanceState);
-        // Этот метод перегружен и имеет два типа параметров, который может принимать...
-        // Какой-нибудь объект или разметку...
-        // Мы этой строчкой говорим, что будем показывать...
         setContentView(R.layout.activity_main);
+        Toast.makeText(
+                MainActivity.this,
+                "onCreate",
+                Toast.LENGTH_SHORT).show();
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +28,50 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(
+                MainActivity.this,
+                "onStart",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(
+                MainActivity.this,
+                "onResume",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(
+                MainActivity.this,
+                "onPause",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(
+                MainActivity.this,
+                "onStop",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(
+                MainActivity.this,
+                "onRestart",
+                Toast.LENGTH_SHORT).show();
     }
 }
