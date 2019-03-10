876fc1e0cc39735c6230ae40de8fbf1a763bbf52
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-10 07:52:27 +0200

    лучший способ обработки нажатия ... с помощью implements Adapter...

--- app/src/main/java/com/omelchenkoaleks/specialist/ListViewActivity.java
+++ app/src/main/java/com/omelchenkoaleks/specialist/ListViewActivity.java
@@ -1,25 +1,19 @@
 package com.omelchenkoaleks.specialist;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ListViewActivity extends ListActivity {
+public class ListViewActivity extends ListActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /**
-         * Так как мы наследуюемся от ListActivity, нам не нужен метод setContentView(),
-         * потому-что ListView уже есть в активити от которого мы наследуемся...
-         */
 
         Resources resources = getResources();
         String[] stationsArray = resources.getStringArray(R.array.stations);
@@ -27,20 +21,11 @@ public class ListViewActivity extends ListActivity {
                 this, R.layout.list_item, stationsArray);
         setListAdapter(stations);
 
-        // мы берем getListView(), потому-что наследуемся от ListActivity:
-        ListView listStations = getListView();
-        listStations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(
-                    AdapterView<?> parent, // передается тот адаптер, на котором мы все делаем
-                    View view, // тот тип вью, который является родительским в item.xml, конкретный на котором было нажатие
-                    int position, // позиция внутри этого списка
-                    long id) { // это может быть уникальный ключ к базе данных, идентификатор по которому можно найти объект
-                CharSequence text = ((TextView) view).getText();
-                int duration = Toast.LENGTH_LONG;
-                Context context = getApplicationContext();
-                Toast.makeText(context, text, duration).show();
-            }
-        });
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
     }
 }
