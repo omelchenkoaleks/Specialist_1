19d94721f2128989b4a3216408695ef01b21f058
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-10 14:32:45 +0200

    ToggleButton and RadioButton как работают...

--- app/src/main/java/com/omelchenkoaleks/specialist/ControlsSampleActivity.java
+++ app/src/main/java/com/omelchenkoaleks/specialist/ControlsSampleActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class ControlsSampleActivity extends AppCompatActivity {
 
@@ -21,5 +23,22 @@ public class ControlsSampleActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Not noted!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Toast.makeText(this, getString(R.string.included),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.switched_off),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        RadioButton radioButton = (RadioButton) view;
+        Toast.makeText(this,
+                getString(R.string.animal_selected_prefix) + " " + radioButton.getText(),
+                Toast.LENGTH_SHORT).show();
     }
 }
