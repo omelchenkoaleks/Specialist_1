5e6c0c7f998a77da0b2b4b31c533a8fcd1b37651
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-10 06:59:51 +0200

    обрабатываем нажатие на элементы списка...

--- app/src/main/java/com/omelchenkoaleks/specialist/ListViewActivity.java
+++ app/src/main/java/com/omelchenkoaleks/specialist/ListViewActivity.java
@@ -1,11 +1,18 @@
 package com.omelchenkoaleks.specialist;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListViewActivity extends ListActivity {
+    private ListView listStantions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,20 @@ public class ListViewActivity extends ListActivity {
         ArrayAdapter<String> stations = new ArrayAdapter<>(
                 this, R.layout.list_item, stationsArray);
         setListAdapter(stations);
+
+        listStantions = getListView();
+        listStantions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(
+                    AdapterView<?> parent,
+                    View view,
+                    int position,
+                    long id) {
+                CharSequence text = ((TextView) view).getText();
+                int duration = Toast.LENGTH_LONG;
+                Context context = getApplicationContext();
+                Toast.makeText(context, text, duration).show();
+            }
+        });
     }
 }
