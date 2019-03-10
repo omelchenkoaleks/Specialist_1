5e51eaf79eac2d53a410017f6011dd793799a577
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-10 15:04:12 +0200

    Используем EditText и очищаем текстовое поле...

--- app/src/main/java/com/omelchenkoaleks/specialist/ControlsSampleActivity.java
+++ app/src/main/java/com/omelchenkoaleks/specialist/ControlsSampleActivity.java
@@ -3,18 +3,42 @@ package com.omelchenkoaleks.specialist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class ControlsSampleActivity extends AppCompatActivity {
+    private EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controls_sample);
+
+        userName = findViewById(R.id.user_name);
+        userName.setOnKeyListener(new View.OnKeyListener() {
+            /**
+             * Семантика этих значений традиционна: true означает,
+             * что событие (event) обработано и больше никаких действий не
+             * требуется, false означает, что событие не обработано этим
+             * обработчиком и будет передано следующим обработчикам в цепочке.
+             */
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Toast.makeText(getApplicationContext(),
+                            userName.getText(), Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void onCheckboxClicked(View view) {
@@ -40,5 +64,9 @@ public class ControlsSampleActivity extends AppCompatActivity {
         Toast.makeText(this,
                 getString(R.string.animal_selected_prefix) + " " + radioButton.getText(),
                 Toast.LENGTH_SHORT).show();
+    }
+
+    public void onCleanText(View view) {
+        userName.setText(null);
     }
 }
