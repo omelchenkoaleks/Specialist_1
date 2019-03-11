e4000a307c5881c77375612355e8da79b97b3603
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-11 14:42:09 +0200

    Передаем и получаем данные через Intent...

--- intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
+++ intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
@@ -3,10 +3,13 @@ package com.omelchenkoaleks.intent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_USER_NAME = "extra userName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickIntent(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        EditText input = findViewById(R.id.userName);
+        String userName = input.getText().toString().trim();
+        if (userName.length() > 0) {
+            Intent intent = new Intent(this, SecondActivity.class);
+            // вложив это таким образом = на приемной стороне мы сможет это вытащить
+            intent.putExtra(EXTRA_USER_NAME, userName);
+            startActivity(intent);
+        }
+        // поле нужно после работы зачистить
+        input.setText(null);
     }
 }
--- intent/src/main/java/com/omelchenkoaleks/intent/SecondActivity.java
+++ intent/src/main/java/com/omelchenkoaleks/intent/SecondActivity.java
@@ -2,7 +2,9 @@ package com.omelchenkoaleks.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+
+        String userName = intent.getStringExtra(MainActivity.EXTRA_USER_NAME);
+        // делаем проверку на null, потому что может вернуться пустое значение
+        userName = (userName == null) ? "Unknown" : userName;
+
+        ((TextView) findViewById(R.id.greeting))
+                .setText(getString(R.string.hello_text) + " " + userName);
     }
 }
