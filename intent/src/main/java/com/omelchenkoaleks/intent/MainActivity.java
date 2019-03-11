aecff300b97c0b8836b8059277d4c5ded7488ec4
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-11 13:28:42 +0200

    простой переход - Intent...

---
+++ intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
@@ -0,0 +1,21 @@
+package com.omelchenkoaleks.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickIntent(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
