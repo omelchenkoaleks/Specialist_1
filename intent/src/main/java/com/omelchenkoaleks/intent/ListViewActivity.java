00bf81fa1a054a35a18398b41dd8a7e0d2f3b34f
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-12 09:54:25 +0200

    получаем данные из другой активити...

---
+++ intent/src/main/java/com/omelchenkoaleks/intent/ListViewActivity.java
@@ -0,0 +1,38 @@
+package com.omelchenkoaleks.intent;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class ListViewActivity extends ListActivity
+        implements AdapterView.OnItemClickListener {
+
+    public static final String EXTRA_SELECTED_STATION = "extra selectedStation";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Resources resources = getResources();
+        String[] stationsArray = resources.getStringArray(R.array.stations);
+        ArrayAdapter<String> stations = new ArrayAdapter<>(
+                this, R.layout.list_item, stationsArray);
+        setListAdapter(stations);
+
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        CharSequence stationSelected = ((TextView) view).getText();
+        Intent result = new Intent();
+        result.putExtra(EXTRA_SELECTED_STATION, stationSelected);
+        setResult(RESULT_OK, result);
+        finish();
+    }
+}
--- intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
+++ intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
@@ -1,20 +1,28 @@
 package com.omelchenkoaleks.intent;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     public static final String EXTRA_USER_NAME = "extra userName";
+    public static final int CODE_SELECT_STATION = 1000;
+    TextView mSelectedStation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSelectedStation = findViewById(R.id.answer_station);
+
     }
 
     public void onClickIntent(View view) {
@@ -28,5 +36,24 @@ public class MainActivity extends AppCompatActivity {
         }
         // поле нужно после работы зачистить
         input.setText(null);
+    }
+
+    public void onChooseStationClick(View view) {
+        Intent intent = new Intent(this, ListViewActivity.class);
+        startActivityForResult(intent, CODE_SELECT_STATION);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CODE_SELECT_STATION) {
+            if (resultCode == RESULT_OK) {
+                mSelectedStation.setText(data
+                        .getStringExtra(ListViewActivity
+                                .EXTRA_SELECTED_STATION));
+            } else {
+                mSelectedStation.setText(getString(R.string.no_station_text));
+            }
+        }
+//        super.onActivityResult(requestCode, resultCode, data);
     }
 }
