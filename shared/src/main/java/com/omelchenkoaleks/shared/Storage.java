6af92d91591e28d71e55500ddf21bd6994873f0b
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-17 11:03:39 +0200

    модифицируем класс Storage ...

--- shared/src/main/java/com/omelchenkoaleks/shared/Storage.java
+++ shared/src/main/java/com/omelchenkoaleks/shared/Storage.java
@@ -7,22 +7,19 @@ class Storage {
     private static final String SHARED_PREFERENCES = "SHARED_PREFERENCES";
     private static final String KEY_STATION_SHARED = "KEY_STATION_SHARED";
 
-    private Context mContext;
     private SharedPreferences mSharedPreferences;
+    private String mDefaultString;
 
     Storage(Context context) {
-        mContext = context;
-        mSharedPreferences = mContext.getSharedPreferences(SHARED_PREFERENCES,
+        mSharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES,
                 Context.MODE_PRIVATE);
+        mDefaultString = context.getResources()
+                .getString(R.string.no_text);
     }
 
     String getStation() {
         String station = mSharedPreferences.getString(KEY_STATION_SHARED, null);
-        if (station == null) {
-            station = mContext.getResources()
-                    .getString(R.string.no_text);
-        }
-        return station;
+        return (station == null) ? mDefaultString : station;
     }
 
     void storeStation(String stationName) {
