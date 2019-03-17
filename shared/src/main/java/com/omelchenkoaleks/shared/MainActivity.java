0033dbbcfdbfb05fb7e759914c0113936123c7f0
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-17 09:11:10 +0200

    SharedPreferences работает )))

--- shared/src/main/java/com/omelchenkoaleks/shared/MainActivity.java
+++ shared/src/main/java/com/omelchenkoaleks/shared/MainActivity.java
@@ -1,17 +1,21 @@
 package com.omelchenkoaleks.shared;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final int CODE_STATION_LIST = 1000;
+    public static final String SHARED_PREFERENCES = "SHARED_PREFERENCES";
+    public static final String KEY_STATION_SHARED = "KEY_STATION_SHARED";
 
+    private SharedPreferences mSharedPreferences;
+    private String mSelectedStation;
     private TextView mNameStationText;
 
     @Override
@@ -20,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mNameStationText = findViewById(R.id.station_text);
+
+        mSharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        mSelectedStation = mSharedPreferences.getString(KEY_STATION_SHARED, null);
+        if (mSelectedStation != null) {
+            mNameStationText.setText(mSelectedStation);
+        } else {
+            mNameStationText.setText(getString(R.string.no_text));
+        }
     }
 
     public void chooseStation(View view) {
@@ -30,13 +42,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == CODE_STATION_LIST) {
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
             if (resultCode == RESULT_OK && data != null) {
                 String selectedStation = data
                         .getStringExtra(ListStationMetroActivity.EXTRA_RESULT_STATION_TEXT_VIEW);
                 mNameStationText.setText(selectedStation);
+                editor.putString(KEY_STATION_SHARED, selectedStation);
             } else {
-                Toast.makeText(this, "NO ...", Toast.LENGTH_SHORT).show();
+                mNameStationText.setText(getString(R.string.no_text));
+                editor.remove(KEY_STATION_SHARED);
             }
+            editor.apply();
         }
     }
 }
