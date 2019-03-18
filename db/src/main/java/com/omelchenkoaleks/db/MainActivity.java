b5372d8e26337a25fd4bd0c592cbf0a6e62d15cd
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-18 12:19:36 +0200

    подредактировал разметку... )

--- db/src/main/java/com/omelchenkoaleks/db/MainActivity.java
+++ db/src/main/java/com/omelchenkoaleks/db/MainActivity.java
@@ -2,15 +2,22 @@ package com.omelchenkoaleks.db;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mInputField;
+    ListView mNotesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInputField = findViewById(R.id.input_field_et);
+        mNotesList = findViewById(R.id.notes_list_lv);
     }
 
     public void onOkButtonClick(View view) {
