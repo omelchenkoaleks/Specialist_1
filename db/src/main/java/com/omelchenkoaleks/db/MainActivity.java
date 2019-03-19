f16a3001ebe1ce7e73d41d88880ad3bbe2f52a1e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-19 09:18:53 +0200

    закрываю базу данных, чтобы она не потребляла лишние ресурсы пока скрыто приложение...

--- db/src/main/java/com/omelchenkoaleks/db/MainActivity.java
+++ db/src/main/java/com/omelchenkoaleks/db/MainActivity.java
@@ -11,15 +11,8 @@ import android.widget.SimpleCursorAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     * заметочка:
-     * запятая указана для указания, что фигурные скобки используются для масива, а
-     * не для выделения блоков...
-     */
     private static final String[] FIELDS = {"_id", DbOpenHelper.COLUMN_NOTE,};
-    // Указываем то поле, которое есть (откуда взять)...
     private static final String[] FROM = {DbOpenHelper.COLUMN_NOTE,};
-    // Указываем по id то место, куда нужно вставить...
     private static final int[] TO = {R.id.note_tv,};
     private static final String ORDER = "_id DESC";
 
@@ -29,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     SimpleCursorAdapter mSimpleCursorAdapter;
     DbOpenHelper mDbOpenHelper = new DbOpenHelper(this);
 
-    // Базу данных нужно получать только тогда, когда она действительно потребуется...
-    // а, когда это? Когда на экране уже что-то нарисовано... onResume()...
     SQLiteDatabase mSQLiteDatabase;
 
     @Override
@@ -43,30 +34,31 @@ public class MainActivity extends AppCompatActivity {
 
         mSimpleCursorAdapter = new SimpleCursorAdapter(
                 this, R.layout.note, null, FROM, TO, 0);
-        /**
-         * КОМЕНТАРИИ ПО ПАРАМЕТРАМ ВЫШЕ:
-         * 1, контекст
-         * 2, специально созданный для адаптера, который он будет использовать для каждой строчки
-         * 3, Cursor пока нет, поэтому написал null
-         * 4, адаптеру нужно знать какие поля из базы данных вставлять в места разметки
-         * 5, адаптеру нужно указать куда вставлять данные - указываем
-         * 6, флаги
-         */
 
-        // привязываем адаптер
         mNotesList.setAdapter(mSimpleCursorAdapter);
     }
 
+    /**
+     * В этом методе мы подключаемся к нашей базе данных, но что будет, если наше приложение
+     * будет скрыто? Она будет продолжать потреблять ресурсы - поэтому...
+     * В методе onStop() мы ее закрываем... null = чтобы сборщик мусора ее почистил...
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        // делаем запрос к базе данных: сначала получаем записываемую базу данных...
         mSQLiteDatabase = mDbOpenHelper.getWritableDatabase();
-        // теперь получаем объект типа Cursor...
         Cursor cursor = mSQLiteDatabase.query(DbOpenHelper.DB_TABLE, FIELDS,
                 null, null,null, null, ORDER);
-        // и прикрепляем курсор к нашему адаптеру...
         mSimpleCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mSQLiteDatabase != null) {
+            mSQLiteDatabase.close();
+            mSQLiteDatabase = null;
+        }
     }
 
     public void onOkButtonClick(View view) {}
