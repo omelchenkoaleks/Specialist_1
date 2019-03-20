0c7b9291550f46f85a1be3f0482b7cb1c0e79a24
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-20 13:28:15 +0200

    HttpUrlConnection = пока набросок ... дальше разбираемся ... )))

---
+++ connection/src/main/java/com/omelchenkoaleks/connection/CurrencyRates.java
@@ -0,0 +1,8 @@
+package com.omelchenkoaleks.connection;
+
+public class CurrencyRates {
+    public final static String KEY_CHAR_CODE = "CharCode";
+    public final static String KEY_VALUE = "Value";
+    public final static String KEY_NOMINAL = "Nominal";
+    public final static String KEY_NAME = "Name";
+}
